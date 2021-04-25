b44cc1a0cf1e8bd1993a2b9851234200e1c1e1c6
kachaeva-ysu <anakuro2511@gmail.com>
2021-04-25 16:12:21 +0300

    FilesAdded

---
+++ app/src/main/java/com/example/calculator/Derivatives.java
@@ -0,0 +1,159 @@
+package com.example.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Derivatives {
+    private List<String> GetOperationsAndOperands(String expression)
+    {
+        List<String> operationsAndOperands = new ArrayList<String>();
+        StringBuilder number = new StringBuilder();
+        boolean previousSymbolIsANumber = false;
+        for(int i=0;i<expression.length();i++)
+        {
+            if (Priority(Character.toString(expression.charAt(i))) == -1 && Character.isLetter(expression.charAt(i))&&expression.charAt(i)!='i')
+            {
+                if(previousSymbolIsANumber) {
+                    operationsAndOperands.add(number.toString());
+                    number=new StringBuilder();
+                    previousSymbolIsANumber=false;
+                    operationsAndOperands.add("*");
+                }
+                if(expression.charAt(i)=='x')
+                {
+                    operationsAndOperands.add("x");
+                    previousSymbolIsANumber=true;
+                }
+                if (expression.charAt(i) == 'p' && expression.charAt(i+1) == 'i')
+                {
+                    i +=1;
+                    operationsAndOperands.add(Double.toString(Math.PI));
+                }
+                else if (expression.charAt(i) == 'e')
+                {
+                    operationsAndOperands.add(Double.toString(Math.E));
+                }
+                else if (expression.charAt(i) == 'a' && expression.charAt(i+1) == 'b' && expression.charAt(i+2) == 's')
+                {
+                    i +=2;
+                    operationsAndOperands.add("abs");
+                }
+                else if (expression.charAt(i) == 's' && expression.charAt(i+1) == 'q' && expression.charAt(i+2) == 'r' && expression.charAt(i + 3) == 't')
+                {
+                    i +=3;
+                    operationsAndOperands.add("sqrt");
+                }
+                else if (expression.charAt(i) == 's' && expression.charAt(i+1) == 'i' && expression.charAt(i+2) == 'n')
+                {
+                    i +=2;
+                    operationsAndOperands.add("sin");
+                }
+                else if (expression.charAt(i) == 'c' && expression.charAt(i+1) == 'o' && expression.charAt(i+2) == 's')
+                {
+                    i +=2;
+                    operationsAndOperands.add("cos");
+                }
+                else if (expression.charAt(i) == 't' && expression.charAt(i+1) == 'g')
+                {
+                    i++;
+                    operationsAndOperands.add("tg");
+                }
+                else if (expression.charAt(i) == 'c' && expression.charAt(i+1) == 't' && expression.charAt(i+2) == 'g')
+                {
+                    i +=2;
+                    operationsAndOperands.add("ctg");
+                }
+                else if (expression.charAt(i) == 'l' && expression.charAt(i+1) == 'n')
+                {
+                    i +=1;
+                    operationsAndOperands.add("ln");
+                }
+                else
+                    throw new IllegalArgumentException();
+            }
+            else if (Priority(Character.toString(expression.charAt(i))) == -1)
+            {
+                number.append(expression.charAt(i));
+                previousSymbolIsANumber = true;
+            }
+            else
+            {
+                if (!number.toString().equals(""))
+                {
+                    operationsAndOperands.add(number.toString());
+                    number=new StringBuilder();
+                    if(expression.charAt(i)=='(')
+                        operationsAndOperands.add("*");
+                }
+                if (expression.charAt(i) == '-' && !previousSymbolIsANumber && (i != 0 && expression.charAt(i-1) != ')' || i == 0))
+                    operationsAndOperands.add("~");
+                else
+                    operationsAndOperands.add(Character.toString(expression.charAt(i)));
+                previousSymbolIsANumber = false;
+            }
+        }
+        if (!number.toString().equals(""))
+            operationsAndOperands.add(number.toString());
+        return operationsAndOperands;
+    }
+    private int Priority(String symbol)
+    {
+        switch (symbol)
+        {
+            case "(": return 0;
+            case ")": return 1;
+            case "+": case "-": return 2;
+            case "*": case "/": return 3;
+            case "~": return 4;
+            case "^":  return 5;
+            case "sqrt": case "sin": case"cos": case "tg": case "ctg": case "ln": case "abs": return 6;
+            default: return -1;
+        }
+    }
+    private List<String> GetReversePolishNotation(String exp)
+    {
+        List<String> operationsAndOperands = GetOperationsAndOperands(exp);
+        Stack<String> stack = new Stack<String>();
+        List<String> rpn = new ArrayList<String>();
+
+        for (String symbol : operationsAndOperands)
+        {
+            int priority = Priority(symbol);
+            if (priority == -1)
+            {
+                rpn.add(symbol);
+            }
+            else
+            {
+                if (symbol.equals("(") || stack.size() == 0 || priority > Priority(stack.peek())&&!symbol.equals(")"))
+                {
+                    stack.push(symbol);
+                }
+                else
+                {
+                    while (stack.size() > 0 && priority <= Priority(stack.peek()))
+                    {
+                        rpn.add(stack.pop());
+                    }
+                    if (symbol.equals(")"))
+                    {
+                        while (Priority(stack.peek()) != 0)
+                        {
+                            rpn.add(stack.pop());
+                        }
+                        stack.pop();
+                    }
+                    else
+                    {
+                        stack.push(symbol);
+                    }
+
+                }
+            }
+        }
+        while(stack.size()!=0)
+            rpn.add(stack.pop());
+        return rpn;
+    }
+}
---
+++ app/src/main/java/com/example/calculator/GCD.java
@@ -0,0 +1,97 @@
+package com.example.calculator;
+
+public class GCD
+{
+    public int[] FindNOD(int a, int b)
+    {
+        int[] answer = new int[3];
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+        int[] stroka1 = new int[7];
+        int[] stroka2 = new int[7];
+        stroka1[1] = 1;
+        stroka1[3] = max;
+        stroka1[5] = 1;
+        stroka1[6] = min;
+        while (stroka1[6] != 0)
+        {
+            stroka2[0] = stroka1[3] / stroka1[6];
+            stroka2[1] = stroka1[4];
+            stroka2[2] = stroka1[5];
+            stroka2[3] = stroka1[6];
+            stroka2[4] = stroka1[1] - stroka2[0] * stroka2[1];
+            stroka2[5] = stroka1[2] - stroka2[0] * stroka2[2];
+            stroka2[6] = stroka1[3] - stroka1[6] * stroka2[0];
+            for (int i = 0; i < 7; i++)
+                stroka1[i] = stroka2[i];
+        }
+        answer[0] = stroka1[3];
+        if (a > b)
+        {
+            answer[1] = stroka1[1];
+            answer[2] = stroka1[2];
+        }
+        else
+        {
+            answer[1] = stroka1[2];
+            answer[2] = stroka1[1];
+        }
+        if(answer[0]<0)
+        {
+            answer[0] *= -1;
+            answer[1] *= -1;
+            answer[2] *= -1;
+        }
+        return answer;
+    }
+    public ComplexNumber[] FindNOD(ComplexNumber a, ComplexNumber b)
+    {
+        ComplexNumber[] answer = new ComplexNumber[3];
+        if (a.GetAbs() < b.GetAbs())
+        {
+            ComplexNumber temp = a;
+            a = b;
+            b = temp;
+        }
+        ComplexNumber[] stroka1 = new ComplexNumber[7];
+        ComplexNumber[] stroka2 = new ComplexNumber[7];
+        stroka1[1] = new ComplexNumber(1,0);
+        stroka1[3] = a;
+        stroka1[5] = new ComplexNumber(1, 0);
+        stroka1[6] = b;
+        stroka1[0] = stroka1[2] = stroka1[4] = new ComplexNumber(0, 0);
+        while (stroka1[6].Re != 0 || stroka1[6].Im != 0)
+        {
+            ComplexNumber q = ComplexNumber.Divide(stroka1[3] , stroka1[6]);
+            q.Re = Math.round(q.Re);
+            q.Im = Math.round(q.Im);
+            stroka2[0] = q;
+            stroka2[1] = stroka1[4];
+            stroka2[2] = stroka1[5];
+            stroka2[3] = stroka1[6];
+            stroka2[4] = ComplexNumber.Subtract(stroka1[1] , ComplexNumber.Multiply(stroka2[0] , stroka2[1]));
+            stroka2[5] = ComplexNumber.Subtract(stroka1[2] , ComplexNumber.Multiply(stroka2[0] , stroka2[2]));
+            stroka2[6] = ComplexNumber.Subtract(stroka1[3] , ComplexNumber.Multiply(stroka1[6] , stroka2[0]));
+            for (int i = 0; i < 7; i++)
+                stroka1[i] = stroka2[i];
+        }
+        answer[0] = stroka1[3];
+        if (a.GetAbs() >= b.GetAbs())
+        {
+            answer[1] = stroka1[1];
+            answer[2] = stroka1[2];
+        }
+        else
+        {
+            answer[1] = stroka1[2];
+            answer[2] = stroka1[1];
+        }
+        if (answer[0].Re < 0||answer[0].Re==0&&answer[0].Im<0)
+        {
+            answer[0] = ComplexNumber.Multiply(answer[0],-1);
+            answer[1] =ComplexNumber.Multiply(answer[1],-1);
+            answer[2] =ComplexNumber.Multiply(answer[2],-1);
+        }
+        return answer;
+    }
+}
